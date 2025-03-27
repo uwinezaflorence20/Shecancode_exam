0284ad56d81b3ea51787fbefba51c658db61a066
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2025-03-27 10:38:33 +0200

    done again

--- question3.js
+++ question3.js
@@ -1,23 +1,46 @@
-// function SumArray(){
-//     let array=[1,2,3,4,5];
-//     let sum=0;
-//     for(let i = 0;i<=array[i] ; i++){
-//       sum=sum+array[i];
+ function SumArray(){
+     let array=[1,2,3,4,5];
+    let sum=0;
+   for(let i = 0;i<=array[i] ; i++){
+       sum=sum+array[i];
 
-//     }
-//     return sum;
-// }
-// let add=SumArray();
-// console.log(add);
+     }
+     return sum;
+ }
+ let add=SumArray();
+console.log(add);
 //q2.b
 
 function recursive(){
-    let number=5;
-    let recurs;
-    for( i=2; i<=number ;i++){
-        recurs = number *i;
+    let number=[0,1];
+    let recurs = 3;
+    for(let i=2; i<=number[i] ;i++){
+        recurs = recurs* number[i];
     } 
-    return number; 
+    return recurs; 
 }
 let answer=recursive();
 console.log(answer);
+
+//c.
+let word="Uwineza";
+console.log(word.reverseString(word));
+
+//d.
+// - iteration help when there is a lot of to be processed compare to the recursion
+
+// e.
+
+function SumArray(){
+    let array=[1,2,3,4,5];
+   let sum=0;
+  for(let i = 0;i<=array[i] ; i++){
+      sum=sum+array[i];
+
+    }
+    return sum;
+}
+let add2=SumArray();
+console.log(add);
+   
+  
--- question4.js
+++ question4.js
@@ -1,8 +1,30 @@
 function StudentsMarks(){
     StudentArray=[10,20,40,70];
     for(let i =0 ; i<=StudentArray[i] ; i++){
-        return StudentArray.filter(StudentArray[i]>20);
+        return StudentArray.filter(StudentArray[i]);
     }
 }
 let answer = StudentsMarks();
 console.log(answer);
+
+//b.
+function SortMarks(){
+    StudentArray=[10,20,40,70];
+    for(let i =0 ; i<=studenArray[i] ; i++){
+        return StudentArray.Sort.ascending();
+    }
+}
+let answer2 = SortsMarks();
+console.log(answer2);
+
+//c.
+
+
+function Duplicate(){
+    StudentArray=[10,10,20,40,70];
+    for(let i =0 ; i<=StudentArray[i] ; i++){
+        return StudentArray.removeDuplicate();
+    }
+}
+let answer3 = Duplicate();
+console.log(answer3);
